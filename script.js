31797d55dfc0f2136f4ca26aa00be4f72c2557b3
ibrahim layth <eibsua@gmail.com>
2026-08-27 01:24:17 -0700

    added logic search for hashtag and insert any hashtag that is make in list

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const formEl = document.querySelector(".form");
 const feedbackListEl = document.querySelector(".feedbacks");
 const submitbtnEl = document.querySelector(".submit-btn");
 const spinnerEl = document.querySelector(".spinner");
+const hashtagEl = document.querySelector('.hashtags');
+
+
 
 const renderFeedbackItem = (feedbackItem)=>{
 const feedbackItemHTML = `
@@ -19,7 +22,7 @@ const feedbackItemHTML = `
                 <p class="feedback__letter">${feedbackItem.badgeLetter}</p>
             </section>
             <div class="feedback__content">
-                <p class="feedback__company">${feedbackItem.company}</p>
+                <p class="feedback__company">${feedbackItem.company.toUpperCase()}</p>
                 <p class="feedback__text">${feedbackItem.text}</p>
             </div>
             <p class="feedback__date">${feedbackItem.daysAgo === 0 ? 'NEW' : `${feedbackItem.daysAgo}d`}</p>
@@ -52,6 +55,32 @@ const className = textCheck === 'valid'? 'form--valid' : 'form--invalid' ;
     }, 2000);
 
 };
+
+
+const isHashtagexist = (company)=>{
+    const hashtagCheck = hashtagEl.querySelectorAll('.hashtag');
+    // console.log(feedbackCheck);
+    let flag = 0;
+    hashtagCheck.forEach(hashtagCheckcmp => {
+
+        if(hashtagCheckcmp.textContent.substring(1).toUpperCase() === company.toUpperCase())
+        {
+            flag = 1;
+        }
+        console.log(hashtagCheckcmp.textContent.substring(1));
+
+
+    })
+
+    if(flag != 1)
+    {
+        const hashtagHTML = `<li class="hashtags__item">
+                <button class="hashtag">#${company.toUpperCase()}</button>
+            </li>`
+        hashtagEl.insertAdjacentHTML("beforeend",hashtagHTML);
+    }
+
+}
 
 const submithandler = (event)=>
 {
@@ -106,6 +135,7 @@ if (text.includes('#') && text.length > 4) {
     textareaEl.value = "";
     submitbtnEl.blur();
     counterEl.textContent = MAXCHARS;
+    isHashtagexist(feedbackItem.company);
       
 
 };
@@ -140,14 +170,54 @@ fetch(`${BASE_API_URL}/feedbacks`).then(res =>{
 }
 ).then(data =>
 {
-    console.log(data);
+    // console.log(data);
     spinnerEl.remove();
     data.feedbacks.forEach(element => {
         renderFeedbackItem(element);
         
     });
+    HashTagupdater();
 }
 ).catch(error =>{
 feedbackListEl.textContent = `Failed to fetch feedback items , Error message : ${error.message}`
 }
 );
+
+const clickHashHandler = (event) => {
+ 
+    const clickHashtag = event.target;
+    const NrHashTag = clickHashtag.closest('.hashtags__item');
+    if(!NrHashTag) return;
+    const HashtagItemEl = NrHashTag.querySelector('.hashtag');
+    if(!HashtagItemEl) return;
+    // console.log(HashtagItemEl.textContent)
+    const feedbackCheck = feedbackListEl.getElementsByClassName('feedback');
+    // console.log(feedbackCheck.length);
+    const feedbacksLength = feedbackCheck.length
+    for(let i=0 ; i<feedbacksLength; i++)
+    {
+        if(!(feedbackCheck[i].querySelector('.feedback__company').textContent.includes(HashtagItemEl.textContent.substring(1))) && !(HashtagItemEl.textContent==="ALL")) {
+            // console.log(!(feedbackCheck[i].querySelector('.feedback__company').textContent));
+            feedbackCheck[i].classList.add('hidden');
+        } else {
+            feedbackCheck[i].classList.remove('hidden');
+        }
+    }
+
+
+
+}
+hashtagEl.addEventListener('click',clickHashHandler)
+
+const HashTagupdater = ()=>{
+
+const feedbackchecker = feedbackListEl.querySelectorAll('.feedback__company');
+feedbackchecker.forEach( feedbackck =>
+{
+    // console.log(feedbackck);
+    isHashtagexist(feedbackck.textContent);
+}
+)
+
+
+} 
